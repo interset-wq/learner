7c52d621916b48eab166639589f21e27a57870bf
interset-wq <intersetwq@qq.com>
2026-06-07 19:33:04 +0800

    feat: extract common Tailwind classes into input.css, render index.html at root
    - Add nav-link, nav-link-mobile, btn-header, btn-header-outline classes
    - Create templates/index.html extending base.html
    - Root route now renders template instead of HttpResponse

--- config/urls.py
+++ config/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 def index(request):
-    return HttpResponse("hello world")
+    return render(request, 'index.html')
 
 
 urlpatterns = [
