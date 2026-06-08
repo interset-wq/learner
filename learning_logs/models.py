44380f2b8c6c45f5506f48ab7437be9b0ea1d6ee
interset-wq <intersetwq@qq.com>
2026-06-08 10:33:31 +0800

    feat: refactor learning_logs - local EasyMDE, entry title, public/private visibility

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -8,23 +8,25 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_public = models.BooleanField(default=False)
 
     def __str__(self):
         return self.text
 
 
 class Entry(models.Model):
+    title = models.CharField(max_length=200, default='Untitled')
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    is_public = models.BooleanField(default=False)
 
     class Meta:
         verbose_name_plural = "entries"
+        ordering = ['-date_added']
 
     def __str__(self):
-        if len(self.text) > 47:
-            return self.text[:50] + '...'
-        return self.text
+        return self.title
 
     @property
     def rendered_text(self):
